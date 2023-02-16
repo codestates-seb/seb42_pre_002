4047a76baf1bb7ea06c1d54d8baa43ae16350847
lapmu <antjdgkdl@gmail.com>
2023-02-16 17:45:16 +0900

    틀 작업 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Questions from './page/Questions';
+import Ask from './page/Ask';
+import Content from './page/Content';
+import Login from './page/Login';
+import Signup from './page/Signup';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <div>
+            <Nav />
+          </div>
+          <div>
+            <Routes>
+              <Route path="/" element={<Questions />} />
+              <Route path="/ask" element={<Ask />} />
+              <Route path="/content" element={<Content />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
