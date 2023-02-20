7300b283ae025f4c0176f69a75fa9a36d8d5f770
formroller <54848570+formroller@users.noreply.github.com>
2023-02-20 13:41:44 +0900

    Replies

--- server/src/main/java/com/codestate/server/replies/entity/Replies.java
+++ server/src/main/java/com/codestate/server/replies/entity/Replies.java
@@ -23,4 +23,35 @@ public class Replies extends BaseEntity{
     @Column(nullable = false)
     private String writer;
 
+    @Enumerated(EnumType.STRING) // 컬럼 맵핑
+    private RepliesStatus repliesStatus = RepliesStatus.REPLIES_REQUEST;
+
+    /*M:1*/
+//    @ManyToOne
+//    @JoinColumn(name="mid")
+//    private Member member;
+//    public void addMember(Member member){
+//        this.member = member;
+//    }
+
+//    @ManyToOne
+//    @JoinColumn(name="qid")
+//    private Question question;
+//    public void addQuestion(Question question){
+//        this.question = question;
+//    }
+
+    // 컬럼 간 매핑
+    @AllArgsConstructor
+    public enum RepliesStatus{
+        REPLIES_REQUEST(1, "댓글 작성"),
+        REPLIES_COMPLETE(2, "댓글 작성 완료"),
+        REPLIES_CANCEL(3, "댓글 작성 취소");
+
+        @Getter
+        private int stepNumber;
+
+        @Getter
+        private String stepDesc;
+    }
 }
